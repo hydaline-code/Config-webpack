90ef508e068d3f2a3a4f3ae8bc79d856975b48bb
hydaline-code <djougancharlen@.com>
2023-07-20 19:53:31 +0100

    Config port 8080

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -4,15 +4,22 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Output Management',
+      template: './src/index.html',
     }),
   ],
   output: {
     filename: '[name].bundle.js',
      path: path.resolve(__dirname, 'dist'),
     clean: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
   },
   module: {
     rules: [
