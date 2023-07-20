1a718d9703a28732efbc0a50044a00cb8ed2a041
hydaline-code <djougancharlen@.com>
2023-07-20 19:09:55 +0100

    Assets management

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,11 +1,18 @@
 
 import _ from 'lodash';
+import './style.scss';
+import icon from './icon.png';
 
 function component() {
   const element = document.createElement('div');
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  const myicon = new Image();
+  myicon.src = './icon.png';
+  element.appendChild(myicon);
 
   return element;
 }
--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -4,7 +4,28 @@ module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+
+    ],
   },
 };
